0810f8b793b8eaf4817d9e4b6192bd39c99efe68
gavinlasher <Krgavin21@yahoo.com>
2022-03-18 17:23:06 -0600

    commit check?

--- Tower/server/services/TicketsService.js
+++ Tower/server/services/TicketsService.js
@@ -2,9 +2,12 @@ import { dbContext } from '../db/DbContext'
 
 class TicketsService {
   async createTicket(body) {
+    const myEvent = await dbContext.TowerEvents.findById(body.eventId)
+    myEvent.capacity--
     const ticket = await dbContext.Tickets.create(body)
     await ticket.populate('event')
     await ticket.populate('account')
+
     return ticket
   }
 }
