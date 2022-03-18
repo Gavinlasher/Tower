ba9e160ce873a46db57d86178259b2006edcd3f6
gavinlasher <Krgavin21@yahoo.com>
2022-03-18 13:46:56 -0600

    first commit

---
+++ Tower/client/app/env.js
@@ -0,0 +1,5 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const domain = 'jz7lvvBXnG4VNZVo8vW6OBdfkhyHIHlw'
+export const audience = 'https://Gavin-dev.com'
+export const clientId = 'jz7lvvBXnG4VNZVo8vW6OBdfkhyHIHlw'
